33e9abd38d59fe3c4239376d906c9a4008ea8f99
Mai <mailen.cardoso@yahoo.com.ar>
2020-01-28 18:31:54 -0300

    Agregue los modelos de nuestro proyecto. Faltan las funciones.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -17,3 +17,39 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Usuario(models.Model):
+    dni = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=45)
+    apellido = models.CharField(max_length=45)
+    direccion = models.CharField(max_length=100)
+    usuario = models.CharField(max_length=10, help_text= "El usuario debe contener entre 6 a 10 caracteres")
+    contrasenia = models.CharField(max_length=20, help_text= "La contraseña debe contener entre 8 a 16 caracteres")
+    telefono = models.CharField(max_length=15)
+    email = models.EmailField()
+
+ 
+
+class Producto(models.Model):
+    codigo = models.AutoField(primary_key=True) #AutoField es un campo autoincremental 
+    precio = models.DecimalField(max_digits=4, decimal_places=2)
+    descripcion = models.TextField()
+    id_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+   
+class Categoria(models.Model):
+    id_categoria = models.AutoField(primary_key=True)
+    descripcion = models.TextField()
+
+
+class Pedido(models.Model):
+    id_pedido = models.AutoField(primary_key=True)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=4, decimal_places=2)
+    fecha_pedido = models.DateTimeField(default=timezone.now)
+    estado = models.ChardField(max_length=100)
+    dni= models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    productos = models.ManyToManyField(Producto) #Se pone asi cuando hay una relacion de muchos a muchos
+
+
+
